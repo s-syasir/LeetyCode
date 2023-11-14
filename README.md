887c97429841e8360315b82f5a4f23af35f82091
yasir <syasir@uw.edu>
2023-11-13 21:35:46 -0800

    quick push

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Just a public repo of my solutions to different problems, etc.
 Adding another line for the commit.
+Adding another line for the commit lol.
--- blind_75/create_files.sh
+++ blind_75/create_files.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+
+# Creating an array of all the folders specifically, so that the template
+# files are copied into the folders only.
 array=($(ls -d */))
+
+# For each of the folders, copy in the template file, renamed
+# to the problem/name of the folder.
 for i in "${array[@]}"
 do
    :
    name=${i::-1}
+   # Create Python file
    cp ./template.py ./$name/$name.py
+   # Create Java files
    cp ./template_main.java ./$name/$name.java
    cp ./template_solution.java ./$name/Solution.java
+   # Create C++ file
    cp ./template.cpp ./$name/$name.cpp
+   # Create C file
    cp ./template.c ./$name/$name.c
+   # Create Go file
+   cp ./template.go ./$name/$name.go
+   # Create Rust file
+   cp ./template.rs ./$name/$name.rs
+   # Create Typescript
+   cp ./template.ts ./$name/$name.ts
 done
